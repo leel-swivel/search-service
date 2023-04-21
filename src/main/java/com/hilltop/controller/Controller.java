f86f847627ad4dea14610a99892bebeaa2305462
leel-swivel <leel@swivelgroup.com.au>
2023-04-21 17:38:26 +0530

    Change unit test

--- src/main/java/com/hilltop/controller/Controller.java
+++ src/main/java/com/hilltop/controller/Controller.java
@@ -2,10 +2,8 @@ package com.hilltop.controller;
 
 import com.hilltop.configuration.Translator;
 import com.hilltop.domain.response.ResponseDto;
-import com.hilltop.enums.ErrorResponseStatusType;
 import com.hilltop.enums.ResponseStatusType;
 import com.hilltop.enums.SuccessResponseStatusType;
-import com.hilltop.wrapper.ErrorResponseWrapper;
 import com.hilltop.wrapper.ResponseWrapper;
 import com.hilltop.wrapper.SuccessResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,19 +32,5 @@ public class Controller {
                 successResponseStatusType, responseDto,
                 translator.toLocale(successResponseStatusType.getCodeString(successResponseStatusType.getCode())), httpStatus);
         return new ResponseEntity<>(successResponseWrapper, httpStatus);
-    }
-
-    /**
-     * This method creates the internal server error response.
-     *
-     * @return response entity
-     */
-    protected ResponseEntity<ResponseWrapper> getInternalServerError() {
-        var errorResponseWrapper = new ErrorResponseWrapper(ResponseStatusType.ERROR,
-                ErrorResponseStatusType.INTERNAL_SERVER_ERROR.getMessage(), null,
-                translator.toLocale(ErrorResponseStatusType.
-                        getCodeString(ErrorResponseStatusType.INTERNAL_SERVER_ERROR.getCode())),
-                ErrorResponseStatusType.INTERNAL_SERVER_ERROR.getCode());
-        return new ResponseEntity<>(errorResponseWrapper, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
